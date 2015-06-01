39206eeccd960e020fb93c35ef908c287d2b778f
michaeld <michaeld@liveperson.com>
2015-06-01 08:37:25 +0300

    fixed up grunt

--- build/aliases.js
+++ build/aliases.js
@@ -1,12 +1,6 @@
 module.exports = function (grunt, options) {
 
-    var buildType = grunt.option('buildType');
-    var buildVersion = grunt.option('buildVersion');
-    var tasks = ['teamcity', 'node_version', 'jshint', 'mochaTest', 'connect', 'lp_blanket_mocha', 'uglify'];
-
-    if ('release' === buildType) {
-        tasks.push('lpRelease');
-    }
+    var tasks = ['node_version', 'jshint', 'mochaTest', 'uglify'];
 
     // computation...
     return {
@@ -14,9 +8,7 @@ module.exports = function (grunt, options) {
         'default': tasks,
         'test': [
             'node_version',
-            'mochaTest',
-            'connect',
-            'lp_blanket_mocha'
+            'mochaTest'
         ]
     };
 };
